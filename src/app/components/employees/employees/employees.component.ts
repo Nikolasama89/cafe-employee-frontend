e03fae629fb8763b9baa9207466d37a0765896d1
Nikos <niqos89@gmail.com>
2025-08-11 22:57:29 +0300

    add employees container component for managing employee-related views

---
+++ src/app/components/employees/employees/employees.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployeeService } from 'src/app/core/services/employee.service';
+import { EmployeeReadOnlyDTO } from 'src/app/shared/interfaces/employee';
+
+@Component({
+  selector: 'app-employees',
+  imports: [CommonModule],
+  templateUrl: './employees.component.html',
+  styleUrl: './employees.component.css'
+})
+export class EmployeesComponent {
+  private employeeService = inject(EmployeeService);
+  employees: EmployeeReadOnlyDTO[] | null = null;
+  error = "";
+
+  ngOnInit() {
+    this.load()
+  }
+
+  load() {
+    this.error = "";
+    this.employeeService.getAll().subscribe({
+      next: (data) => (this.employees = data),
+      error: (err) => (this.error = err?.error?.message ?? "Request failed")
+    });
+  }
+}
