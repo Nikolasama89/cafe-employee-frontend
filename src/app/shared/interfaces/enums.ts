6b986c31bc7631463ff232363f89822c47165b6b
Nikos <niqos89@gmail.com>
2025-08-11 22:55:34 +0300

    add enums for consistent across application

---
+++ src/app/shared/interfaces/enums.ts
@@ -0,0 +1,12 @@
+export enum JobTitle {
+  BARISTA = "BARISTA",
+  BARMAN = "BARMAN",
+  WAITER = "WAITER",
+  CLEANER = "CLEANER",
+  CASHIER = "CASHIER"
+}
+
+export enum Role {
+  ADMIN = "ADMIN",
+  EMPLOYEE = "EMPLOYEE"
+}
