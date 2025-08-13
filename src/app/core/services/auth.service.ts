275fab00af4eb18da830770f5b2d865570e1698c
Nikos <niqos89@gmail.com>
2025-08-14 00:40:45 +0300

    feat: create auth service with jwt decode, login, logout functionality

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,19 +1,66 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal, effect } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { AuthenticationRequestDTO, AuthenticationResponseDTO } from 'src/app/shared/interfaces/auth';
+import { jwtDecode } from 'jwt-decode';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.development';
+import { AuthenticationRequestDTO, AuthenticationResponseDTO, LoggedInUser, Role } from 'src/app/shared/interfaces/auth';
+
+const API_URL_AUTH = `${environment.apiURL}/auth`
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private http = inject(HttpClient)
-  private baseUrl = "http://localhost:8080/api/auth"
+  private router = inject(Router)
 
-  login(payload: AuthenticationRequestDTO) {
-    return this.http.post<AuthenticationResponseDTO>(`${this.baseUrl}/authenticate`, payload)
+  user$ = signal<LoggedInUser | null>(null)
+
+  constructor() {
+    const token = localStorage.getItem("access_token")
+    if (token) {
+      const p = jwtDecode<{username:string; role?: Role}>(token)
+      this.user$.set({username:p.username, role: (p.role ?? "EMPLOYEE") as Role})
+    }
+
+    effect(() => {
+      const u = this.user$();
+      console.log(u ? `User logged in: ${u.username}`: "No user")
+    })
   }
 
-  saveToken(token: string) {
-    localStorage.setItem("token", token)
+  login(credentials: AuthenticationRequestDTO) {
+    return this.http.post<AuthenticationResponseDTO>(`${API_URL_AUTH}/authenticate`, credentials)
+  }
+
+  setSessionAndUser(auth: AuthenticationResponseDTO) {
+    localStorage.setItem("access_token", auth.token)
+    const p = jwtDecode<{username: string; role?: Role}>(auth.token)
+    this.user$.set({username: p.username, role: (p.role ?? "EMPLOYEE") as Role})
+  }
+
+  logout() {
+    this.user$.set(null)
+    localStorage.removeItem("access_token")
+    this.router.navigate(["login"])
+  }
+
+  isTokenExpired(): boolean {
+    const token = localStorage.getItem("access_token")
+    if (!token){
+      return true;
+    } 
+    try {
+      const decoded = jwtDecode< {exp?: number}>(token)
+      const now = Math.floor(Date.now()/ 1000)
+      return !decoded.exp || decoded.exp < now
+
+    } catch {
+      return true;
+    }
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem("access_token")
   }
 }
