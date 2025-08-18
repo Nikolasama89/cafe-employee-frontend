3b5a18fdaac5c1fa9b8649c968a7a0c54e24fa4c
Nikos <niqos89@gmail.com>
2025-08-18 23:17:28 +0300

    feat: add header with nav

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, RouterLinkActive, NgIf, NgTemplateOutlet],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  private auth = inject(AuthService)
+
+  user = this.auth.user$
+
+  isAdmin(): boolean {
+    return this.user()?.role === "ADMIN"
+  }
+
+  logout(): void {
+    this.auth.logout()
+  }
+}
