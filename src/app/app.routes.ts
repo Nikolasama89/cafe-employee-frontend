3606e1b4f10b4433a12bf35b4124f79affd92f17
Nikos <niqos89@gmail.com>
2025-08-18 22:24:07 +0300

    feat: add authGuard for seeing employees

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { EmployeesComponent } from './components/employees/employees/employees.component';
 import { LoginComponent } from './components/auth/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent},
-  { path: 'employees', component: EmployeesComponent },
+  { path: 'employees', component: EmployeesComponent, canActivate: [authGuard] },
   { path: "**", redirectTo: "login"}
 ];
