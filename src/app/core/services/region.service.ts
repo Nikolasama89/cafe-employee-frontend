788830e8c888e552c22181423a5517453b9a69ee
Nikos <niqos89@gmail.com>
2025-08-23 21:33:13 +0300

    fix: typo in Region

---
+++ src/app/core/services/region.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Region } from 'src/app/shared/interfaces/region';
+import { environment } from 'src/environments/environment.development';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegionService {
+
+  private http = inject(HttpClient)
+  private readonly baseUrl = `${environment.apiURL}/regions`;
+
+  getAll(): Observable<Region[]> {
+    return this.http.get<Region[]>(this.baseUrl)
+  }
+}
