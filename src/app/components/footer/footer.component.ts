da9c2036c5b3d6ae25118a3a3b5832219488de65
Nikos <niqos89@gmail.com>
2025-08-29 21:10:14 +0300

    feat: add footer component

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome'
+import { faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-footer',
+  imports: [FontAwesomeModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+  githubIcon = faGithub
+  linkedInIcon = faLinkedin
+
+  currentYear: number = new Date().getFullYear();
+
+}
