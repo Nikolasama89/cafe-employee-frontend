479acfe6b91db945fc07b64ed0a5a13dcf268066
Nikos <niqos89@gmail.com>
2025-08-31 14:13:08 +0300

    feat: add route for Employee detail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { roleGuard } from './core/guards/role.guard';
 import { UserCreateComponent } from './components/users/user-create/user-create.component';
 import { EmployeeCreateComponent } from './components/employees/employee-create/employee-create.component';
 import { EmployeeUpdateComponent } from './components/employees/employee-update/employee-update.component';
+import { EmployeeDetailComponent } from './components/employees/employee-detail/employee-detail.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -16,5 +17,6 @@ export const routes: Routes = [
   { path: 'users/new', component: UserCreateComponent, canActivate: [authGuard, roleGuard], data: { roles: ['ADMIN'] } },
   { path: 'employees/new', component: EmployeeCreateComponent, canActivate: [authGuard, roleGuard], data: { roles: ['ADMIN'] } },
   { path: 'employees/:id/edit', component: EmployeeUpdateComponent, canActivate: [authGuard, roleGuard], data: { roles: ['ADMIN'] } },
+  { path: 'employees/:id', component: EmployeeDetailComponent},
   { path: "**", redirectTo: "login"}
 ];
